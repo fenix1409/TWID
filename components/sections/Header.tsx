bef8b6872a35a0c56a27d35e5809169c16ec29d7
fenix1409 <b2550486@gmail.com>
2025-03-09 10:39:39 +0500

    Add Header component, implement React Query provider, and configure custom fonts

---
+++ components/sections/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='px-[56px] bg-[#A5AAAD] border-b-[1px] border-black'></header>
+  )
+}
+
+export default Header
